eb898f86de2550545e1e98331ef274b320ad9223
Tony Lau <99046505+intrepidcanadian@users.noreply.github.com>
2023-12-02 17:56:21 -0500

    submission

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import "./App.scss";
 import ToDoWrapper from "./components/ToDoWrapper";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
 
   return (
+  <div>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<ToDoWrapper />} />
+      </Routes>
+    </Router>
+  </div>
   
-        <ToDoWrapper />
 
   );
 }
